43164ee0f584559f92fd72b896f14fdc7984b1b3
a1000rhan <1000rhan@gmail.com>
2022-03-27 14:56:06 +0300

    jj

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.listen(8001, () => {
+app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
 });
