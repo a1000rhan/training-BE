110bd41b2d971b748cc7c9e14e341e73a7811b93
TaherNaqi <eng-naqi@hotmail.com>
2022-03-30 14:32:48 +0300

    profilesFitch

--- api/profiles/profiles.controllers.js
+++ api/profiles/profiles.controllers.js
@@ -1,9 +1,7 @@
 const Profile = require("../../db/models/Profile");
 exports.fetchProfile = async (req, res, next) => {
   try {
-    const profile = await Profile.findOne({ owner: req.user._id }).populate(
-      "courses"
-    );
+    const profile = await Profile.findOne({ owner: req.user._id });
     res.status(200).json(profile);
   } catch (error) {
     next(error);
@@ -12,9 +10,11 @@ exports.fetchProfile = async (req, res, next) => {
 exports.fetchProfiles = async (req, res, next) => {
   try {
     if (req.user.type === "admin") {
-      const profiles = await Profile.find()
+      const profiles = await Profile.find({ $concatArrays: ["students"] })
         .where("courses.profileStatus")
-        .equals("pending");
+        .equals("pending")
+        .populate("courses.courseId");
+
       res.status(200).json(profiles);
     } else res.status(401).json({ message: "You are not authorized" });
   } catch (error) {
